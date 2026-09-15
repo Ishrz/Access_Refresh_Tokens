6ec93a73bf9d2efeb9934cc30e2a5cbb2fd0c902
Ishrz <ishrzz66@gmail.com>
2026-09-15 14:25:50 +0530

    refactor:folder restrucuture

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,6 +11,11 @@ app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}))
 
 
 app.use("/api/v1/auth", authRouter)
